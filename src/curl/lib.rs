ec7f18d334fd7741f188d3b0175c00ff1aa9bf71
Andelf <andelf@gmail.com>
2014-01-19 23:52:40 +0800

    adding source code.

---
+++ src/curl/lib.rs
@@ -0,0 +1,23 @@
+#[desc = "A rust package for libcurl."];
+#[license = "MIT"];
+
+
+use std::libc::c_char;
+use std::c_str::CString;
+use std::path::BytesContainer;
+use std::str;
+
+
+#[link(name = "curl")]
+extern {
+    fn curl_version() -> *c_char;
+}
+
+
+pub fn version() -> ~str {
+    unsafe {
+        // for curl version, we don't own it
+        let cver = CString::new(curl_version(), false);
+        str::from_utf8_owned(cver.container_into_owned_bytes())
+    }
+}
