369963bf8dabe31a2f38ce128954953c33e54009
Andelf <andelf@gmail.com>
2014-01-20 23:37:34 +0800

    add some easy interface, add lib to gitignore

--- src/curl/easy.rs
+++ src/curl/easy.rs
@@ -1,28 +1,86 @@
-#[desc = "curl easy interface"];
-#[license = "MIT"];
-
-use std::libc::{c_void, uintptr_t};
+use std::libc::{uintptr_t, c_int, c_char};
 use std::c_str::CString;
 use std::path::BytesContainer;
 use std::str;
 
 #[link(name = "curl")]
 extern {
+    fn curl_easy_escape(h: uintptr_t, url: *c_char, length: c_int) -> *c_char;
     fn curl_easy_init() -> uintptr_t;
+    fn curl_easy_cleanup(h: uintptr_t);
+    fn curl_easy_duphandle(h: uintptr_t) -> uintptr_t;
+    fn curl_easy_perform(h: uintptr_t) -> c_int;
+    fn curl_easy_reset(h: uintptr_t);
+    fn curl_easy_strerror(code: c_int) -> *c_char;
+    fn curl_easy_setopt(h: uintptr_t, option: c_int, parameter: uintptr_t) -> c_int;
+    fn curl_free(ptr: *c_char);
 }
 
 pub struct Curl {
-    pub handle: uintptr_t,
+    handle: uintptr_t,
 }
 
 impl Curl {
     pub fn is_null(&self) -> bool {
         self.handle == 0
     }
-}
 
+    // FIXME: handle \x00 byte in string
+    pub fn escape(&self, url: &str) -> ~str {
+        let c_url = url.to_c_str();
+        c_url.with_ref(|c_buf| {
+                unsafe {
+                    let ret = curl_easy_escape(self.handle, c_buf, url.len() as c_int);
+                    let escaped_bytes = CString::new(ret, false).container_into_owned_bytes();
+                    curl_free(ret);
+                    str::from_utf8_owned(escaped_bytes)
+                }
+            })
+    }
+
+    pub fn init() -> Curl {
+        let hd = unsafe { curl_easy_init() };
+        Curl { handle: hd }
+    }
+
+    pub fn cleanup(&self) {
+        unsafe { curl_easy_cleanup(self.handle) }
+    }
+
+    pub fn duphandle(&self) -> Curl {
+        let ret = unsafe { curl_easy_duphandle(self.handle) };
+        Curl { handle: ret }
+    }
+
+    pub fn perform(&self) -> int {
+        let ret = unsafe { curl_easy_perform(self.handle) };
+        ret as int
+    }
+
+    pub fn reset(&self) {
+        unsafe { curl_easy_reset(self.handle) }
+    }
+}
 
 pub fn init() -> Curl {
     let hd = unsafe { curl_easy_init() };
     Curl { handle: hd }
+}
+
+pub fn cleanup(handle: Curl) {
+    let hd = handle.handle;
+    unsafe { curl_easy_cleanup(hd) }
+}
+
+
+pub fn perform(handle: Curl) {
+    let hd = handle.handle;
+    unsafe { curl_easy_perform(hd) };
+}
+
+pub fn strerror(code: int) -> ~str {
+    unsafe {
+        let cver = CString::new(curl_easy_strerror(code as c_int), false);
+        str::from_utf8_owned(cver.container_into_owned_bytes())
+    }
 }
--- src/curl/test.rs
+++ src/curl/test.rs
@@ -11,6 +11,55 @@ fn test_version() {
 #[test]
 fn test_easy_init() {
     let h = curl::easy::init();
-    println!("h={}", h.handle);
-    //assert!(&curl::easy::init().is_null())
+    assert!(!curl::easy::init().is_null());
+    curl::easy::cleanup(h)
+}
+
+#[test]
+fn test_easy_curl_init() {
+    let c = curl::easy::Curl::init();
+    assert!(!c.is_null());
+    c.cleanup()
+}
+
+#[test]
+fn test_easy_curl_perform() {
+    let c = curl::easy::Curl::init();
+    let ret = c.perform();
+    assert!(ret == 3);
+    c.cleanup();
+}
+
+#[test]
+fn test_easy_strerror() {
+    assert!(curl::easy::strerror(0) == ~"No error");
+    assert!(curl::easy::strerror(3).len() > 0);
+}
+
+#[test]
+fn test_easy_escape() {
+    let c = curl::easy::Curl::init();
+    assert_eq!(c.escape("abcEFG"), ~"abcEFG");
+    assert_eq!(c.escape("&*()"), ~"%26%2A%28%29");
+    // c.escape("\x00fuck"));
+    c.cleanup();
+}
+
+#[test]
+fn test_easy_duphandle() {
+    let c = curl::easy::Curl::init();
+    assert!(!c.is_null());
+    let cc = c.duphandle();
+    assert!(!c.is_null());
+    c.cleanup();
+    cc.cleanup();
+}
+
+
+#[test]
+fn test_easy_reset() {
+    let c = curl::easy::Curl::init();
+    c.reset();
+    assert!(!c.is_null());
+    c.cleanup();
 }
